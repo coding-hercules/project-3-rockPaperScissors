4e7b92ecf363e712c65f1b429edb683ba6870a94
Dillon Tan Zhao Kai <dillon.tanzhaokai@gmail.com>
2022-08-16 13:56:16 +0800

    Create function to determine winner/loser in each round

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const getComputerChoice = (playerSelection, computerSelection) => {
+    
+    if (playerSelection === 'rock') {
+        switch (computerSelection) {
+            case 'rock':
+                return 'Draw!';
+                break;
+            case 'paper':
+                return 'You Lose!';
+                break;
+            case 'scissors':
+                return 'You Win!';
+                break;
+        }
+    } else if (playerSelection === 'paper') {
+        switch (computerSelection) {
+            case 'rock':
+                return 'You Win!';
+                break;
+            case 'paper':
+                return 'Draw!';
+                break;
+            case 'scissors':
+                return 'You Lose!';
+                break;
+        }
+    } else if (playerSelection === 'scissors') {
+        switch (computerSelection) {
+            case 'rock':
+                return 'You Lose!';
+                break;
+            case 'paper':
+                return 'You Win!';
+                break;
+            case 'scissors':
+                return 'Draw!';
+                break;
+        }
+    } else {
+
+    }
+}
